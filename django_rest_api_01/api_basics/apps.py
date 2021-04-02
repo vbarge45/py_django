89446fa5cf239e3ad1429c26f345b01cb85778fd
vbarge45 <vbarge45@gmail.com>
2021-04-02 09:17:17 -0700

    first commit

--- django_rest_api_01/api_basics/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ApiBasicsConfig(AppConfig):
-    name = 'api_basics'
--- pro_todo/app_todo/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppTodoConfig(AppConfig):
-    name = 'app_todo'
